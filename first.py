f0cd1b8b18c7b6b9b00eb46981fa54a3325c30cb
Daniel T. Margul <margul@nyu.edu>
2015-07-05 03:29:07 -0400

    ok

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 import csv
-
+natoms = 1536
 xyzfile = open('wxyz.csv','r')
 xyzdata = xyzfile.read()
 xyzrows = xyzdata.split('\n')
@@ -19,14 +19,22 @@ outfile.write(
 	"\n"
 	"\usepackage{fontspec}"
 	"\n"
-	"\begin{tikzpicture}"
+	"\ begin{document}"
+	"\n"
+	"\ begin{tikzpicture}"
 	)
 xyzstuff = []
 for row in xyzrows:
 	split_xyzrow = row.split(" , ")
 	xyzstuff.append(split_xyzrow)
 
+i = 1
+
 for row in xyzstuff:
+	try:
+			row[1]
+	except IndexError:
+    		break
 	if row[1] == 'O':
 		outfile.write(
 			"\draw[black,thick," 
@@ -38,4 +46,38 @@ for row in xyzstuff:
 			)
 		outfile.write(row[2])
 		outfile.write(",")
-		outfile.write("\n")
+		outfile.write(row[3])
+		outfile.write(",")
+		outfile.write(row[4])
+		outfile.write(
+			")"
+			"coordinate(O)" 
+			"circle [radius=.73cm] ;"
+			"\n"
+				)
+	if row[1] == 'H':
+		outfile.write(
+			"\draw[black,thick," 
+			"shading = axis,circle," 
+			"left color=gray!100," 
+			"right color=white!100!,"
+			"shading angle=45]"
+			"("
+			)
+		outfile.write(row[2])
+		outfile.write(",")
+		outfile.write(row[3])
+		outfile.write(",")
+		outfile.write(row[4])
+		outfile.write(
+			")"
+			"coordinate(O)" 
+			"circle [radius=.37cm] ;"
+			"\n"
+			)
+	i = i + 1
+outfile.write(
+	"\end{tikzpicture}"
+	"\n"
+	"\end{document}"
+	)
