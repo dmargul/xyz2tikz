6918df3165a836baf0e023efd0a7c39d4cdf7b77
Daniel T. Margul <margul@nyu.edu>
2015-07-05 12:18:13 -0400

    CHNO

--- first.py
+++ first.py
@@ -1,13 +1,17 @@
 import csv
-natoms = 1536
-xyzfile = open('wxyz.csv','r')
-xyzdata = xyzfile.read()
-xyzrows = xyzdata.split('\n')
+
+
+C_options = ['C','CA','CB','CAT','CAY','CY']
+H_options = ['H','HN','HB1','HB2','HB3','HT1',
+			  'HT2','HT3','HNT','HY1','HY2','HY3']
+O_options = ['O','OT','OY']
+N_options = ['N','NT']
+
 outfile = open('tikz_out.tex','w')
 outfile.write(
-	"\documentclass{article}"
+	"\documentclass[tightpage]{standalone}"
 	"\n"
-	"\usepackage[letterpaper,landscape]{geometry}"
+	"\usepackage{varwidth}"
 	"\n"
 	"\usepackage{tikz}"
 	"\n"
@@ -23,19 +27,22 @@ outfile.write(
 	"\n"
 	"\ begin{tikzpicture}"
 	)
+
+xyzfile = open('wxyz.csv','r')
+xyzdata = xyzfile.read()
+xyzrows = xyzdata.split('\n')
 xyzstuff = []
 for row in xyzrows:
 	split_xyzrow = row.split(" , ")
 	xyzstuff.append(split_xyzrow)
 
-i = 1
 
 for row in xyzstuff:
 	try:
 			row[1]
 	except IndexError:
     		break
-	if row[1] == 'O':
+	if row[1] in O_options:
 		outfile.write(
 			"\draw[black,thick," 
 			"shading = axis,circle," 
@@ -55,7 +62,7 @@ for row in xyzstuff:
 			"circle [radius=.73cm] ;"
 			"\n"
 				)
-	if row[1] == 'H':
+	if row[1] in H_options:
 		outfile.write(
 			"\draw[black,thick," 
 			"shading = axis,circle," 
@@ -71,11 +78,50 @@ for row in xyzstuff:
 		outfile.write(row[4])
 		outfile.write(
 			")"
-			"coordinate(O)" 
+			"coordinate(H)" 
 			"circle [radius=.37cm] ;"
 			"\n"
 			)
-	i = i + 1
+	if row[1] in C_options:
+		outfile.write(
+			"\draw[black,thick," 
+			"shading = axis,circle," 
+			"left color=black!100," 
+			"right color=gray!100!,"
+			"shading angle=45]"
+			"("
+			)
+		outfile.write(row[2])
+		outfile.write(",")
+		outfile.write(row[3])
+		outfile.write(",")
+		outfile.write(row[4])
+		outfile.write(
+			")"
+			"coordinate(H)" 
+			"circle [radius=.77cm] ;"
+			"\n"
+			)
+	if row[1] in N_options:
+		outfile.write(
+			"\draw[black,thick," 
+			"shading = axis,circle," 
+			"left color=blue!100," 
+			"right color=blue!70!,"
+			"shading angle=45]"
+			"("
+			)
+		outfile.write(row[2])
+		outfile.write(",")
+		outfile.write(row[3])
+		outfile.write(",")
+		outfile.write(row[4])
+		outfile.write(
+			")"
+			"coordinate(H)" 
+			"circle [radius=.75cm] ;"
+			"\n"
+			)						
 outfile.write(
 	"\end{tikzpicture}"
 	"\n"
